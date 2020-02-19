0b02a9001cdef48e3844d0f1bb2587bee6a1029b
rajatthakur00@gmail.com <rajat@55>
2020-02-19 18:40:50 +0530

    Initial Commit

---
+++ src/main/java/com/example/brewer_client/BrewerClientApplication.java
@@ -0,0 +1,14 @@
+package com.example.brewer_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewerClientApplication.class, args);
+        System.out.println("***Starting****");
+    }
+
+}
---
+++ src/test/java/com/example/brewer_client/BrewerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brewer_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewerClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
