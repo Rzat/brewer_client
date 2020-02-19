2e06c59390843c1c8c9eb18e4fb00bf4a5d0f3e7
rajatthakur00@gmail.com <rajat@55>
2020-02-19 19:34:45 +0530

    Calling master service using RestClient

---
+++ src/main/java/com/example/brewer_client/web/client/BreweryClient.java
@@ -0,0 +1,30 @@
+package com.example.brewer_client.web.client;
+
+import com.example.brewer_client.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(value = "micro.brewery", ignoreUnknownFields = false) //prefix
+public class BreweryClient {
+    public String BEER_PATH_V1 = "/api/v1/beer/";
+    private String apiHost;
+
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID uuid) {
+        return restTemplate.getForObject(apiHost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+    }
+
+    public void setApiHost(String apiHost) {
+        this.apiHost = apiHost;
+    }
+}
