d0d12076102cd374c7ff299b612fb4009a5bc7de
rajatthakur00@gmail.com <rajat@55>
2020-02-19 19:57:16 +0530

    Post with RestTemplate

--- src/test/java/com/example/brewer_client/web/client/BreweryClientTest.java
+++ src/test/java/com/example/brewer_client/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -20,4 +21,16 @@ class BreweryClientTest {
         BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
         assertNotNull(beerDto);
     }
+
+    @Test
+    void testSaveNewBeer() {
+        //given
+        BeerDto beerDto = BeerDto.builder().id(UUID.randomUUID()).beerName("Testing").build();
+        URI uri = breweryClient.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+
+
+    }
+
 }
